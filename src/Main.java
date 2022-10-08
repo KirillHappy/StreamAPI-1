b40ccd84ffdf2c0c324c4c3a7e3139205cdfdf49
Kirill Happy <schastlivyj1986@gmail.com>
2022-10-08 19:36:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.function.Predicate.not;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long min18 = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println(min18);
+
+
+        List<String> listFamily = persons.stream()
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= 27)
+                .filter(person -> person.getSex().equals(Sex.MAN))
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        System.out.println(listFamily);
+
+        List<Person> readyList = persons.stream()
+                .filter(education -> education.getEducation().equals(Education.HIGHER))
+                .filter(woman -> woman.getSex().equals(Sex.WOMAN) && woman.getAge() >= 18 && woman.getAge() <= 60)
+                .filter(man -> man.getSex().equals(Sex.MAN) && man.getAge() >= 18 && man.getAge() <= 65)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println(readyList);
+
+
+    }
+}
